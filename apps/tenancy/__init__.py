dfaf2e153c6bf511a6e68ab542dcdd8743b5e6d8
Aspek01 <alisonerpk@gmail.com>
2026-02-04 21:53:27 +0300

    fix: avoid AppRegistryNotReady by keeping tenancy init lightweight (D-1.4)

--- apps/tenancy/__init__.py
+++ apps/tenancy/__init__.py
@@ -2,10 +2,12 @@ from apps.tenancy.context import (
     set_active_scope,
     get_active_company_id,
     require_active_company_id,
+    clear_active_scope,
 )
 
 __all__ = [
     "set_active_scope",
     "get_active_company_id",
     "require_active_company_id",
+    "clear_active_scope",
 ]
